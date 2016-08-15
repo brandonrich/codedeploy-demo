6b66a1cb40a4cb065c96bfe93c46980c56afeb0d
Brandon Rich <brich@nd.edu>
2016-08-15 15:16:02 -0400

    install nginx

--- codedeploy/scripts/configure_nginx.sh
+++ codedeploy/scripts/configure_nginx.sh
@@ -1,2 +1,2 @@
-sudo service nginx stop
+sudo yum install nginx -y
 sudo ln -s -f /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive /opt/current-deployment
