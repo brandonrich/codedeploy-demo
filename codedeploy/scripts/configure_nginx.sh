22e5c49dc309ba65bbcc64fee73f0301371d9ed1
Brandon Rich <brich@nd.edu>
2016-08-15 14:48:21 -0400

    force symbolic link

--- codedeploy/scripts/configure_nginx.sh
+++ codedeploy/scripts/configure_nginx.sh
@@ -1,2 +1,2 @@
 sudo service nginx stop
-sudo ln -s /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOY_ID /opt/current-deployment
+sudo ln -s -f /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOY_ID /opt/current-deployment
