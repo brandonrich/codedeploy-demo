5edb2d1a03498b029c73f00a31abfcbb104bb5e6
Brandon Rich <brich@nd.edu>
2016-08-17 22:05:10 -0400

    resolve bundle command not found by sourcing bash profile

--- codedeploy/scripts/start_unicorn.sh
+++ codedeploy/scripts/start_unicorn.sh
@@ -1,4 +1,5 @@
 source /home/ec2-user/.bash_profile
+source /home/ec2-user/.bashrc
 cd /opt/current-deployment
 bundle
 bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
