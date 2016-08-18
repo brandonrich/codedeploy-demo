61485b726116d00609c94d79edb01078e74b9deb
Brandon Rich <brich@nd.edu>
2016-08-17 22:59:44 -0400

    trying to get nokogiri to install

--- codedeploy/scripts/start_unicorn.sh
+++ codedeploy/scripts/start_unicorn.sh
@@ -1,10 +1,7 @@
-#export HOME=/home/ec2-user
-#export PATH=$PATH:$HOME/.local/bin:$HOME/bin
 . /home/ec2-user/.bash_profile
 . /home/ec2-user/.bashrc
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
-#source /home/ec2-user/.bash_profile
-#source /home/ec2-user/.bashrc
 cd /opt/current-deployment
+bundle config build.nokogiri --use-system-libraries
 bundle
 bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
