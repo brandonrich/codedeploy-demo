b0404e94cdc9421d59c8ec7da86cf3130b5bf1cd
Brandon Rich <brich@nd.edu>
2016-08-17 22:29:40 -0400

    trying more ways to get path right

--- codedeploy/scripts/start_unicorn.sh
+++ codedeploy/scripts/start_unicorn.sh
@@ -1,5 +1,9 @@
-source /home/ec2-user/.bash_profile
-source /home/ec2-user/.bashrc
+#export HOME=/home/ec2-user
+#export PATH=$PATH:$HOME/.local/bin:$HOME/bin
+bin/bash -c 'source /home/ec2-user/.bash_profile'
+bin/bash -c 'source /home/ec2-user/.bashrc'
+#source /home/ec2-user/.bash_profile
+#source /home/ec2-user/.bashrc
 cd /opt/current-deployment
 bundle
 bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
