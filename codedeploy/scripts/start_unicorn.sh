27733850622c2c0f7d1400537be08cecec854bb1
Brandon Rich <brich@nd.edu>
2016-08-17 21:38:26 -0400

    _

--- codedeploy/scripts/start_unicorn.sh
+++ codedeploy/scripts/start_unicorn.sh
@@ -1,3 +1,3 @@
 cd /opt/current-deployment
 bundle
-unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
+bundle exec unicorn -E development -c /opt/current-deployment/config/unicorn.rb -D
