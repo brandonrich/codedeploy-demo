c6276b510b779d04b02a42e4a6c013d3d8e2f6ac
Brandon Rich <brich@nd.edu>
2016-08-17 22:33:39 -0400

    trying more ways to get path right

--- codedeploy/scripts/start_unicorn.sh
+++ codedeploy/scripts/start_unicorn.sh
@@ -2,6 +2,7 @@
 #export PATH=$PATH:$HOME/.local/bin:$HOME/bin
 . /home/ec2-user/.bash_profile
 . /home/ec2-user/.bashrc
+export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 #source /home/ec2-user/.bash_profile
 #source /home/ec2-user/.bashrc
 cd /opt/current-deployment
